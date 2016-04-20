00a137d6fd6c5ba508b0d4bdf00d46a7a6728d52
Frostao <chawk9621@gmail.com>
2016-04-20 12:55:12 -0400

    added ssh

--- app.js
+++ app.js
@@ -20,15 +20,39 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/random.text', function (req, res) {
-        res.send('random.text');
-        });
+	res.send('random.text');
+});
 
 app.post('/test-post', function(req, res) {
-         var host = req.body.host;
-         var port = req.body.port;
-         res.send("host is " + host);
-         res.send("port is " + port);
-         });
+	var host = req.body.host;
+	var port = req.body.port;
+	var username = req.body.username;
+	var password = req.body.password;
+
+	var Client = require('ssh2').Client;
+
+	var conn = new Client();
+	conn.on('ready', function() {
+		console.log('Client :: ready');
+		conn.exec('ls', function(err, stream) {
+			if (err) throw err;
+			stream.on('close', function(code, signal) {
+				console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+				conn.end();
+			}).on('data', function(data) {
+				res.send(data);
+				console.log('STDOUT: ' + data);
+			}).stderr.on('data', function(data) {
+				console.log('STDERR: ' + data);
+			});
+		});
+	}).connect({
+		host: host,
+		port: port,
+		username: username,
+		password: password
+	});
+});
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
@@ -40,5 +64,5 @@ var appEnv = cfenv.getAppEnv();
 app.listen(appEnv.port, '0.0.0.0', function() {
 
 	// print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+	console.log("server starting on " + appEnv.url);
 });
