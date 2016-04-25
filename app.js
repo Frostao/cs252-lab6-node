ed34b9d6b50bc1a2c103339bcf110b762f4c2dd9
Caleb Belth <cbelth@purdue.edu>
2016-04-25 09:21:30 -0400

    setup elephantsql

--- app.js
+++ app.js
@@ -164,3 +164,24 @@ app.post('/connect', function(req, res) {
 	}
 });
 
+/* ElephantSQL */
+var pg = require('pg');
+//or native libpq bindings
+//var pg = require('pg').native
+
+var conString = process.env.ELEPHANTSQL_URL || "postgres://nouhpzho:kxh5OnjhtkIG_bpoOhkjIlDtTat6_vIK@pellefant.db.elephantsql.com:5432/nouhpzho";
+
+var client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+    client.end();
+  });
+});
